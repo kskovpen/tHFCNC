b9583012edf257c68e6f71c0fd4be9846260b200
kskovpen <Kirill.Skovpen@cern.ch>
2015-10-28 11:59:07 +0100

    tool to calculate initial statistics

---
+++ NtupleProducer/test/getEntries/getEntriesRoot.C
@@ -0,0 +1,18 @@
+void getEntriesRoot(std::string filein)
+{
+   gROOT->SetBatch();
+   
+   float nw0 = 0;
+   float nc0 = 0;
+   
+   TFile *f = TFile::Open(filein.c_str());
+   TH1F* h_weight = (TH1F*)f->Get("FlatTree/hweight");
+   nw0 += h_weight->GetBinContent(1);
+   TH1F* h_count = (TH1F*)f->Get("FlatTree/hcount");
+   nc0 += h_count->GetBinContent(1);
+   f->Close();
+
+   std::cout << nw0 << " " << nc0 << std::endl;
+      
+   gApplication->Terminate();
+}
