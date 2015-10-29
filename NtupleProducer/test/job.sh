c24ff807fca6851a3d942d7fc0bf6ce6dbf082bf
kskovpen <Kirill.Skovpen@cern.ch>
2015-10-29 11:30:08 +0100

    update

--- NtupleProducer/test/job.sh
+++ NtupleProducer/test/job.sh
@@ -1,3 +1,36 @@
-#!/bin/env sh
+#!/bin/sh
 
-cp -r ${1} ${2}
+export X509_USER_PROXY=/home-pbs/kskovpen/proxy/x509up_u7989
+
+export LD_PRELOAD=/usr/lib64/libglobus_gssapi_gsi.so.4
+
+WDIR=$(pwd)
+
+line2=${line2}
+fout=${fout}
+isdata=${isdata}
+noe=${noe}
+xsec=${xsec}
+dout=${dout}
+sample=${sample}
+
+export ROOTSYS=/cvmfs/cms.cern.ch/slc6_amd64_gcc491/lcg/root/6.02.00-odfocd4
+ls $ROOTSYS/bin/thisroot.sh
+source $ROOTSYS/bin/thisroot.sh
+rootV=$(root-config --version)
+echo "ROOT v${rootV} has been set up"
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${dout}/../
+
+export LD_LIBRARY_PATH=\
+/cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/gcc/4.9.1-cms/lib64:\
+/usr/lib64:\
+/cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/gcc/4.9.1-cms/lib:\
+/cvmfs/cms.cern.ch/slc6_amd64_gcc491/cms/cmssw/CMSSW_7_4_6/external/slc6_amd64_gcc491/lib/:\
+/cvmfs/cms.cern.ch/slc6_amd64_gcc491/cms/cmssw/CMSSW_7_4_6/lib/slc6_amd64_gcc491/:\
+$LD_LIBRARY_PATH
+
+export PATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/gcc/4.9.1-cms/bin:$PATH
+
+echo "Executing .././NtupleProducer --file ${line2} --outfile ${dout}${fout} --isdata ${isdata} --noe ${noe} --xsec ${xsec} --nmax ${nmax}"
+${dout}/./NtupleProducer --file ${line2} --outfile ${dout}${fout} --isdata ${isdata} --noe ${noe} --xsec ${xsec} --nmax ${nmax}
