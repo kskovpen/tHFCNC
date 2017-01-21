bd784ac4bda8b949328710f2e4862c90a1f825f7
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2017-01-21 11:52:16 +0100

    update

--- Limit/cards/combineCards.zsh
+++ Limit/cards/combineCards.zsh
@@ -3,17 +3,17 @@
 rm -rf comb_*.txt
 
 combineCards.py \
-b2j3=input_MVAHut_b2j3_hut.txt \
-b2j4=input_MVAHut_b2j4_hut.txt \
-b3j3=input_MVAHut_b3j3_hut.txt \
-b3j4=input_MVAHut_b3j4_hut.txt \
-b4j4=input_MVAHut_b4j4_hut.txt \
-> comb_MVAHut_hut.txt
+b2j3=input_MVAHutMAX_b2j3_hut.txt \
+b2j4=input_MVAHutMAX_b2j4_hut.txt \
+b3j3=input_MVAHutMAX_b3j3_hut.txt \
+b3j4=input_MVAHutMAX_b3j4_hut.txt \
+b4j4=input_MVAHutMAX_b4j4_hut.txt \
+> comb_MVAHutMAX_hut.txt
 
 combineCards.py \
-b2j3=input_MVAHct_b2j3_hct.txt \
-b2j4=input_MVAHct_b2j4_hct.txt \
-b3j3=input_MVAHct_b3j3_hct.txt \
-b3j4=input_MVAHct_b3j4_hct.txt \
-b4j4=input_MVAHct_b4j4_hct.txt \
-> comb_MVAHct_hct.txt
+b2j3=input_MVAHctMAX_b2j3_hct.txt \
+b2j4=input_MVAHctMAX_b2j4_hct.txt \
+b3j3=input_MVAHctMAX_b3j3_hct.txt \
+b3j4=input_MVAHctMAX_b3j4_hct.txt \
+b4j4=input_MVAHctMAX_b4j4_hct.txt \
+> comb_MVAHctMAX_hct.txt
