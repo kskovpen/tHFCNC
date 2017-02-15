12be363dbc426aef6e11fdb1fe851bcef5be288b
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2017-02-15 10:23:13 +0100

    update

--- NtupleAnalyzer/test/MVA/run.zsh
+++ NtupleAnalyzer/test/MVA/run.zsh
@@ -1,11 +1,25 @@
 #!/bin/env zsh
 
+./runMVA HutSTallb2j3
+./runMVA HctSTallb2j3
+./runMVA HutTTallb2j3
+./runMVA HctTTallb2j3
+
+./runMVA HutSTallb2j4
+./runMVA HctSTallb2j4
 ./runMVA HutTTallb2j4
 ./runMVA HctTTallb2j4
+
+./runMVA HutSTallb3j3
+./runMVA HctSTallb3j3
+./runMVA HutTTallb3j3
+./runMVA HctTTallb3j3
+
 ./runMVA HutSTallb3j4
 ./runMVA HctSTallb3j4
 ./runMVA HutTTallb3j4
 ./runMVA HctTTallb3j4
+
 ./runMVA HutSTallb4j4
 ./runMVA HctSTallb4j4
 ./runMVA HutTTallb4j4
