87f4e1c40bbf99fcf8f8da1ddd528e02f098590b
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2017-07-01 15:15:23 +0200

    data cards and input files for final fits

---
+++ Limit/FinalFits/cards/combineCards.zsh
@@ -0,0 +1,18 @@
+#!/bin/env zsh
+
+rm -rf comb_*.txt
+
+combineCards.py \
+b2j3=input_MVAHutComb_b2j3_hut.txt \
+b2j4=input_MVAHutComb_b2j4_hut.txt \
+b3j3=input_MVAHutComb_b3j3_hut.txt \
+b3j4=input_MVAHutComb_b3j4_hut.txt \
+> comb_MVAHutComb_hut.txt
+
+combineCards.py \
+b2j3=input_MVAHctComb_b2j3_hct.txt \
+b2j4=input_MVAHctComb_b2j4_hct.txt \
+b3j3=input_MVAHctComb_b3j3_hct.txt \
+b3j4=input_MVAHctComb_b3j4_hct.txt \
+b4j4=input_MVAHctComb_b4j4_hct.txt \
+> comb_MVAHctComb_hct.txt
